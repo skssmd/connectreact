65c895ae9bf9383410cc92b416a7ca73c420d309
Shahariar Munir <96967895+skssmd@users.noreply.github.com>
2025-03-08 15:25:34 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+server: {
+    allowedHosts: [
+      'conn-6sq5.onrender.com',  // Add your Render URL here
+      'localhost',               // Allow localhost for local development
+      '127.0.0.1',               // Add more if needed
+    ],
   plugins: [react(), tailwindcss(),],
   
   
